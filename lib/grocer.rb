6e479df50751d9301579034462bd3ab007f6159a
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-10-15 20:14:59 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -8,21 +8,26 @@ def find_item_by_name_in_collection(name, collection)
         end
       end
     end
-  nil
+   nil
 end
 
 def consolidate_cart(cart)
-  consolidated_cart = {}
-    cart.each do |k|
-      k.each do |innerkey, innervalue|
-        if find_item_by_name_in_collection(innervalue, consolidated_cart) == nil
-          consolidated_cart = k.merge({:count => 1})
-          #binding.pry
-        else innervalue == consolidated_cart[:item]
-          consolidated_cart[:count] += 1
-        end
-      end
+  new_cart = []
+  counter = 0
+  while counter < cart.length
+    new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
+    if new_cart_item != nil
+      new_cart_item[:count] += 1
+    else
+      new_cart_item = {
+        :item => cart[counter][:item],
+        :price => cart[counter][:price],
+        :clearance => cart[counter][:clearance],
+        :count => 1
+      }
+      new_cart << new_cart_item
     end
-  consolidated_cart
+    counter += 1
+  end
+  new_cart
 end
-#end
